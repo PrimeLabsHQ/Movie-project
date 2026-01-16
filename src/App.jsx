c600ef6c3a0813ec18905205dabe94d505e7e9a4
Prime Labs <mrcody497@gmail.com>
2026-01-16 21:02:59 +0500

    feat:add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-  
-
   return (
     <div>
-  <Navbar />
-	<div className="home">
-		<form>
-			<input className="searchInput" placeholder="Search for a movie..." />
-			<button type="submit">Search 🔎</button>
-		</form>
-		<div className="movie-list">
-			<div className="movie-card">
-				<img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
-				<h3>The Avengers</h3>
-				<p>2012</p>
-				<a href="movie-detail.html" data-discover="true">Details</a>
-			</div>
-			<div className="movie-card">
-				<img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
-				<h3>The Avengers</h3>
-				<p>2012</p>
-				<a href="movie-detail.html" data-discover="true">Details</a>
-			</div>
-			<div className="movie-card">
-				<img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
-				<h3>The Avengers</h3>
-				<p>2012</p>
-				<a href="movie-detail.html" data-discover="true">Details</a>
-			</div>
-		</div>
-	</div>	
+      <Navbar />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import {useState,useRef,useEffect} from 'react'
+
+
+export default function Home() {
+  return (
+    <div className="home">
+		<form>
+			<input className="searchInput" placeholder="Search for a movie..." />
+			<button type="submit">Search 🔎</button>
+		</form>
+		<div className="movie-list">
+			<div className="movie-card">
+				<img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
+				<h3>The Avengers</h3>
+				<p>2012</p>
+				<a href="movie-detail.html" data-discover="true">Details</a>
+			</div>
+			<div className="movie-card">
+				<img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
+				<h3>The Avengers</h3>
+				<p>2012</p>
+				<a href="movie-detail.html" data-discover="true">Details</a>
+			</div>
+			<div className="movie-card">
+				<img alt="The Avengers" src="https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg" />
+				<h3>The Avengers</h3>
+				<p>2012</p>
+				<a href="movie-detail.html" data-discover="true">Details</a>
+			</div>
+		</div>
+	</div>	
+  )
+}
