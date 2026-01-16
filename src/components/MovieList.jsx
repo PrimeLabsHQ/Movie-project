002e3f780be589d664bce14a3a0ce046dd49c140
Prime Labs <mrcody497@gmail.com>
2026-01-16 21:16:22 +0500

    tweak:create Movie list component

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+export default function MovieList() {
+  return (
+    <div className="movie-list">
+        <MovieCard />       
+      </div>
+  )
+}
