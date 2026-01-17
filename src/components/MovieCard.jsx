ef9b4436b9d15ae4e7927b01942fbed3bec58afe
Prime Labs <mrcody497@gmail.com>
2026-01-17 14:09:01 +0500

    feat:add detail page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function MovieCard({movie}) {
+export default function MovieCard({ movie }){
   return (
     <div className="movie-card">
-          <img
-            alt={movie.Title}
-            src={movie.Poster}
-          />
-          <h3>{movie.Title}</h3>
-          <p>{movie.Year}</p>
-          <Link to={`/movie/${movie.imdbID}`} data-discover="true">
-            Details
-          </Link>
-        </div>
-  )
+      <img alt={movie.Title} src={movie.Poster} />
+      <h3>{movie.Title}</h3>
+      <p>{movie.Year}</p>
+      <Link to={`/movie/${movie.imdbID}`}>Details</Link>
+    </div>
+  );
 }
---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,9 @@
+import {useParams} from 'react-router-dom';
+import { useEffect,useState } from 'react';
+export default function MovieDetail() {
+    const {id} = useParams();
+    const [movie,setMovie]=useState(null)
+  return (
+    
+  )
+}
